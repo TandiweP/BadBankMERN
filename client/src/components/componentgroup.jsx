0130b3f7d5d47893b8b33c88596bf6eb4ae017df
BillieP <87207713+TandiweP@users.noreply.github.com>
2022-04-19 16:47:39 -0600

    comment out

--- client/src/components/componentgroup.jsx
+++ client/src/components/componentgroup.jsx
@@ -1,20 +1,20 @@
-import Navbar from "./Navbar";
-import Home from "./home";
-import CreateAccount from "./CreateAccount";
-import Login from "./Login";
-import Deposit from "./Deposit"
-import Withdraw from "./Withdraw"
-import Logout from "./Logout"
-import AllData from "./AllData";
+// import Navbar from "./Navbar";
+// import Home from "./home";
+// import CreateAccount from "./CreateAccount";
+// import Login from "./Login";
+// import Deposit from "./Deposit"
+// import Withdraw from "./Withdraw"
+// import Logout from "./Logout"
+// import AllData from "./AllData";
 
 
-export {
-    Navbar,
-    Home,
-    CreateAccount,
-    Login,
-    Logout,
-    Deposit,
-    Withdraw,
-    AllData
-}
+// export {
+//     Navbar,
+//     Home,
+//     CreateAccount,
+//     Login,
+//     Logout,
+//     Deposit,
+//     Withdraw,
+//     AllData
+// }
