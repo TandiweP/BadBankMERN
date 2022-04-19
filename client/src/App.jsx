447826f8fabf5229bc14e36193edd4ad8cc2543a
BillieP <87207713+TandiweP@users.noreply.github.com>
2022-04-19 15:51:05 -0600

    added console.log loggedIn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,6 @@ import { React, useState } from 'react';
 import {Routes,  Route} from "react-router-dom";
 import   Navbar from './components/Navbar';
 import ContextProvider from './components/context';
-// import  Home  from './components/home';
-// import  CreateAccount  from './components/CreateAccount';
-// import  Deposit  from './components/Deposit';
-// import  Withdraw  from './components/Withdraw';
-// import  Login  from './components/Login';
-// import  Logout  from './components/Logout';
-// import  AllData  from './components/AllData';
 import CurrencyConverter from './components/currency/Currency'
 
 import {
@@ -30,9 +23,11 @@ export default function App() {
 
   const handleLogin = () => {
     setLoggedIn(true);
+    console.log("User {loggedIn});
   }
   const handleLogout = () => {
     setLoggedIn(false);
+     console.log("User {loggedIn});
   }
 
   return (
