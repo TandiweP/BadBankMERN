93d3cd8f59c8858c519e72571b295a7cd33f8190
BillieP <87207713+TandiweP@users.noreply.github.com>
2022-04-19 16:15:05 -0600

    Update Deposit.jsx

--- client/src/components/Deposit.jsx
+++ client/src/components/Deposit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from './Card'
 
 export default function Deposit(){
@@ -7,7 +7,17 @@ export default function Deposit(){
     const [deposit, setDeposit]                 = useState('');
     const [account, setAccount]                 = useState({});
     const email = localStorage.getItem('email');
-    
+    const loggedIn = localStorage.getItem('email') != null;
+    const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (!loggedIn) 
+      navigate("/login");
+    else {
+      console.log("status: loggedin")
+    }
+  }, []);
 
     function getAccount() {
       const url = `/account/find/${email}`;
